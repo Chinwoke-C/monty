1fe8c9fab4de46a82d9f3faca3359e6fe9548ca1
Chinwoke <canugwara99@gmail.com>
2023-12-14 09:16:58 -0800

    commit

--- read_file.c
+++ read_file.c
@@ -17,37 +17,43 @@ void read_file(char *filename, stack_t **stack)
 
 	var_global.file = fopen(filename, "r");
 
-    if (var_global.file == NULL) {
-        fprintf(stderr, "Error: Can't open file %s\n", filename);
-        exit(EXIT_FAILURE);
-    }
+	if (var_global.file == NULL)
+	{
+	fprintf(stderr, "Error: Can't open file %s\n", filename);
+	exit(EXIT_FAILURE);
+	}
 
-    while (fgets(buffer, MAX_BUFFER_SIZE, var_global.file) != NULL) {
-        /* Remove trailing newline if present*/
-        size_t len = strlen(buffer);
-        if (len > 0 && buffer[len - 1] == '\n') {
-            buffer[len - 1] = '\0';
-        }
+	while (fgets(buffer, MAX_BUFFER_SIZE, var_global.file) != NULL)
+	{
 
-        line = parse_line(buffer, stack, line_count);
-        if (line == NULL || line[0] == '#') {
-            line_count++;
-            continue;
-        }
+	size_t len = strlen(buffer);
 
-        s = get_op_func(line);
-        if (s == NULL) {
-            fprintf(stderr, "L%d: unknown instruction %s\n", line_count, line);
-            exit(EXIT_FAILURE);
-        }
+	if (len > 0 && buffer[len - 1] == '\n')
+	{
+	buffer[len - 1] = '\0';
+	}
 
-        s(stack, line_count);
-        line_count++;
-    }
+	line = parse_line(buffer, stack, line_count);
 
-    fclose(var_global.file);
+	if (line == NULL || line[0] == '#')
+	{
+	line_count++;
+	 continue;
+	}
+	s = get_op_func(line);
+	if (s == NULL)
+	{
+	fprintf(stderr, "L%d: unknown instruction %s\n", line_count, line);
+	exit(EXIT_FAILURE);
+	}
+
+	s(stack, line_count);
+	line_count++;
+	}
+
+	fclose(var_global.file);
 }
-	
+
 
 /**
  * get_op_func -  checks opcode and returns the correct function
