babb0e1adb137cd2a41b80f918dd1a6643bc900b
Chinwoke <canugwara99@gmail.com>
2023-12-14 08:17:19 -0800

    commit

--- 1_pint.c
+++ 1_pint.c
@@ -7,14 +7,13 @@
 void pint(stack_t **stack, unsigned int line_number)
 {
 stack_t *runner;
-	
+
 runner = *stack;
 if (runner == NULL)
 {
 (void)line_number;
 fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
 exit(EXIT_FAILURE);
-
 }
 
 printf("%d\n", runner->n);
