5af400df2c63f70a190afed66d37cbb1a13d2ce7
Chinwoke <canugwara99@gmail.com>
2023-12-14 02:54:38 -0800

    bytecodes

--- 1_pint.c
+++ 1_pint.c
@@ -3,9 +3,11 @@
 /**
  * pint - prints the value at the top of the stack, followed by a new line.
  * @stack: Pointer to the stack
+ * @line_number: number of line opcode occurs on
  */
-void pint(Stack_t *stack)
+void pint(stack_t **stack,unsigned in line_number)
 {
+	stack_t *top
 	if (stack->top == NULL)
 	{
 		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
