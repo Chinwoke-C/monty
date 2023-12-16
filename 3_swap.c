1d8aac301900900eb30e24a7df37df2d872ea6ec
ItumelengRandyMalau <itumelengRandy5@gmail.com>
2023-12-16 01:00:48 -0800

    3_swap.c

---
+++ 3_swap.c
@@ -0,0 +1,32 @@
+#include "monty.h"
+/**
+ *_swap - swaps the top most 2 elements of the stack
+ * @stack: input
+ * Return: void
+ */
+void swap(Stack **stack, unsigned int line_number)
+{
+	if (stack->top == NULL || stack->top->next == NULL)
+	{
+		fprintf(stderr, "Error: can't print, stack too short\n");
+		exit(EXIT_FAILURE);
+	}
+
+	Node *first = stack->top;
+	Node *second = first->next;
+
+	/* Adjust links for the first node*/
+	first->next = second->next;
+	if (second->next != NULL)
+	{
+		second->next->prev = first;
+	}
+
+	/*Adjust links for the second node*/
+	second->prev = NULL;
+	second->next = first;
+	first->prev = second;
+
+	/* Update the top of the stack*/
+	stack->top = second;
+}
